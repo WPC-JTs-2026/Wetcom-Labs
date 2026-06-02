c261acc89b366ef91e6f3d0c5107eb280bd1292f
Patricio Ibar <patricio.ibar@wetcom.com>
2026-06-02 09:32:08 -0300

    despliegue ESXi con datastore local + despliegue de vCenter

---
+++ terraform/stage1/.terraform.lock.hcl
@@ -0,0 +1,45 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/vsphere" {
+  version     = "2.4.3"
+  constraints = "~> 2.4.0"
+  hashes = [
+    "h1:8sP/KmlvMN6TPKpuHwBtDlxUOnMcR3082AQZAU6ewzQ=",
+    "h1:PMfKJa/q7SJXujpZkrLFtPSxiyt6EZ3b+ojcdvM5qjE=",
+    "zh:01cd380635c9727eeb06cfdeaf9aaf9187bc418c8c5ae8baf3b7ae8e20df0ba9",
+    "zh:0bd03b1f2c119c95b9c049a9082d8a918624bf1f136029090dfc2bf5dd8748b9",
+    "zh:0fa82a384b25a58b65523e0ea4768fa1212b1f5cfc0c9379d31162454fedcc9d",
+    "zh:491f60d6398f6b3b4f995ff3d037d9965c5ca6a6df531a1a4046c8b01b6a19dd",
+    "zh:4c5e457ce1f700afe97a4cf261a109e94605afd7333ae9210b29e13702030bff",
+    "zh:84bbd781eef094cf467fc2e51d6cf0ecb89917c3aff56a360c6e442a2ae9a05c",
+    "zh:863b512c635fd4ef29c657dfcdac1626dd2ef0ef19dedce7aa741e94d8d603ff",
+    "zh:8d162916dad3461f88d8e68691c8a0b1c0007976633766fa54d1cc3f01c1c4a7",
+    "zh:912dd30fe3e747c0640532a8709f7922857c14d6ad63f822165f2ac2077fbd78",
+    "zh:d0ee547c5706a9c8d081080960efb287220694775ef717016597ffffdbf56352",
+    "zh:d350b5a698b5f0ba719fcdc42295094065a70c5ced50d4e78308cf65b0186846",
+    "zh:d5b10ed1e15367e51d77f5d512b3547559bf67ffbeefee050fb5d19a0309e95f",
+  ]
+}
+
+provider "registry.terraform.io/vmware/vsphere" {
+  version     = "2.16.0"
+  constraints = "~> 2.15"
+  hashes = [
+    "h1:drrM5abcYhf5nZuTLRtgrECKx9n/pOXXXGkmF2EonIY=",
+    "zh:04922e5403d8cf4ef77420e8f9f7237b822cd8995dead87e2627e287ad8f2301",
+    "zh:1a2e8faa46e37e73425ff9785f0f46195676314440556c42730b93437a8252d6",
+    "zh:2d5a8ff7002ac32dc8c05c56403410200b8f3398c26ceba6a005c12f0d92b788",
+    "zh:4f0ff87729cf453858472e529b4b5edd65bd886a7854207e195736a3e23127a6",
+    "zh:6d550f528fbc0a13558d5b719faf5cc0ce2203962c1a7f6b59327585703371b1",
+    "zh:789c00cdcd207d95667e294e0dd7266432dcaa94b9a81cc972388efd93595a8d",
+    "zh:7ddc676fa8805d80cff8447116c76b000a9270cea5c1c960dd34cbbe360ffec6",
+    "zh:8262df7c3ea58564e89f8d52d900fdd0c1c2e594a5ec07c7a781589d817f259e",
+    "zh:99e5df2e561b6984eb245feb1737542dbd4f3dc6cfe83672268e34ce3c417ab0",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:b61d511ada5efdb14336c6ff8fa7eea73e3ba50bf67d535153830de7c4e83fe0",
+    "zh:d897781524f4493c94115155fe71b6d73feb0718e5ba6a362f94b91811b67928",
+    "zh:e3363005f4248fb41eb92c4eca2a4bfbbe2cd91ad44bc40c1ec775199855b8fc",
+    "zh:f4e81e188bd2c82921c6811336c6df46bb53fb84ac44c5e3e5e2117bde3f44ab",
+  ]
+}
---
+++ terraform/stage2/main.tf
@@ -0,0 +1,105 @@
+# Archivo para desplegar la VCSA usando el instalador CLI desde la ISO
+
+resource "local_file" "vcsa_json" {
+  filename = "${path.module}/vcsa.json"
+  content = jsonencode({
+    "__version": "2.13.0",
+    "ceip": {
+      "settings": {
+        "ceip_enabled": false
+      }
+    },
+    "new_vcsa": {
+      "esxi": {
+        "hostname": "${var.esxi_deploy_host}",
+        "username": "root",
+        "password": "${var.esxi_password}",
+        "deployment_network": "VM Network",
+        "datastore": "datastore1"
+      },
+      "appliance": {
+        "thin_disk_mode": true,
+        "deployment_option": "tiny",
+        "name": "vcsa-nested"
+      },
+      "network": {
+        "ip_family": "ipv4",
+        "mode": "static",
+        "ip": var.vcsa_ip,
+        "dns_servers": [var.vcsa_dns],
+        "prefix": "24",
+        "gateway": var.vcsa_gateway,
+        "system_name": var.vcsa_fqdn
+      },
+      "os": {
+        "password": var.vcsa_password,
+        "ntp_servers": "1.pool.ntp.org",
+        "ssh_enable": true
+      },
+      "sso": {
+        "password": var.vcsa_password,
+        "domain_name": "vsphere.local"
+      }
+    }
+  })
+}
+
+resource "null_resource" "deploy_vcsa" {
+  depends_on = [
+    local_file.vcsa_json,
+  ]
+
+  # Trigger cada vez que cambie el archivo json o si no existe
+  triggers = {
+    json_hash = md5(local_file.vcsa_json.content)
+  }
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      set -euo pipefail
+      jsonPath="${path.module}/vcsa.json"
+      extractPoint="${var.vcsa_exctract_path}"
+      installerPath="$extractPoint/vcsa-cli-installer/lin64/vcsa-deploy.bin"
+
+      # Validaciones previas
+      if [ ! stat extractPoint >/dev/null 2>&1 ]; then
+        echo "La carpeta de extracción $extractPoint no existe"
+        echo "Por favor, asegúrate de que la ruta sea correcta y que se haya extraido la ISO."
+        exit 1
+      fi
+      if [ ! stat "$installerPath" >/dev/null 2>&1 ]; then
+        echo "El instalador CLI no se encontró en $installerPath"
+        echo "Por favor, asegúrate de que la ISO se haya extraido correctamente y que el instalador esté presente."
+        exit 1
+      fi
+
+      # Agregar dependencias y permisos de ejecución al instalador y sus bibliotecas
+      export LD_LIBRARY_PATH="$extractPoint/vcsa-cli-installer/lin64:$extractPoint/vcsa-cli-installer/lin64/lib:$extractPoint/vcsa-cli-installer/lin64/lib/deps"
+      chmod +x "$LD_LIBRARY_PATH/"*.so || true
+      find /home/wpc/vcsa_iso_extract -name "*.bin" -o -name "ovftool" | xargs chmod +x || true
+      chmod +x "$installerPath" || true
+
+      # Esperar a que el host ESXi responda antes de intentar la instalación
+      IPDestino="${var.esxi_deploy_host}"
+        PuertoSSH=22
+      SegundosEspera=10
+        echo "Esperando a que el host ESXi $IPDestino acepte conexiones en el puerto SSH..."
+        while true; do
+          if timeout 5 bash -c ">/dev/tcp/$IPDestino/$PuertoSSH" >/dev/null 2>&1; then
+            echo "[OK] ¡El equipo $IPDestino acepta conexiones en el puerto $PuertoSSH!"
+            break
+          fi
+          TiempoActual="$(date +%H:%M:%S)"
+          echo "[$TiempoActual] Sin respuesta en puerto $PuertoSSH de $IPDestino. Reintentando en $SegundosEspera segundos..."
+          sleep "$SegundosEspera"
+        done
+
+      echo "Ejecutando instalador en $installerPath ..."
+      "$installerPath" install --no-ssl-certificate-verification --no-esx-ssl-verify --accept-eula --acknowledge-ceip "$jsonPath"
+
+      echo "vCenter instalado exitosamente."
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
